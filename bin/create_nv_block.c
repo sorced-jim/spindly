d693905834d712736aba7487422874743c946807
Jim Morrison <jim@twist.com>
2012-01-30 14:36:57 -0800

    Fix a minor size_t issue

--- bin/create_nv_block.c
+++ bin/create_nv_block.c
@@ -46,7 +46,7 @@ int main(int argc, char *argv[])
   char *nv_block_data = NULL;
   size_t nv_block_data_size = 0;
   spdy_nv_block_pack(&nv_block_data, &nv_block_data_size, &nvb);
-  printf("Size: %d\n", nv_block_data_size);
+  printf("Size: %zd\n", nv_block_data_size);
   for(i = 0; i < nv_block_data_size; i++) {
     printf("%02x ", nv_block_data[i]);
     /* Printed 8 bytes, make a newline. */
